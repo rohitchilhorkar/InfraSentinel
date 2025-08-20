00ab47f8e5a89b169b0e804f1bf5514df7ee39a7
Rohit Chilhorkar <78499149+rohitchilhorkar@users.noreply.github.com>
2025-08-21 01:43:46 +0530

    Creation of Sentinel's Infra

---
+++ InfraSentinal/main.tf
@@ -0,0 +1,40 @@
+# Create VPC
+module "vpc"{
+    source = "terraform-aws-modules/vpc/aws"
+    version = "5.5.1"
+
+    name = "${var.cluster-name}-vpc"
+    cidr = "10.0.0.0/16"
+    region = "${var.region}"
+    azs = ["${var.region}a", "${var.region}b"]
+
+    private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+    public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+    # configures the module to provision a single, 
+    # shared NAT Gateway across all private subnets within the VPC.
+    enable_nat_gateway = true
+    single_nat_gateway = true
+}
+
+# Creating EKS cluster
+module "eks"{
+    source = "terraform-aws-modules/eks/aws"
+    version = "20.8.5
+
+    cluster_name = "${var.cluster-name}"
+    cluster_version = "1.30"
+
+    vpc_id = module.vpc.vpc_id
+    subnet_ids = module.vpc.private_subnets 
+
+    eks_managed_node_groups = {
+        default = {
+            desired_size = var.node_group_size
+            max_size = 3
+            min_size = 1
+
+            instance_type = ["t3.medium"]
+        }
+    }
+}
---
+++ InfraSentinal/variable.tf
@@ -0,0 +1,11 @@
+variable "region" {
+    default  = "us-east-1"
+}
+
+variable "cluster-name" {
+    default  = "InfraSentinel"
+}
+
+variable "node_group_size"{
+    default  = 2
+}
